754a78576a14083aa4aeee5fa8ea8c07ef8d7f1e
Alexander Schellenberg <alexander.schellenberg@mindcurv.com>
2016-02-28 15:10:37 +0100

    Reformatted code.

--- src/koward/Main.java
+++ src/koward/Main.java
@@ -10,54 +10,19 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Options;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
 
 /**
  * Java M2 Converter Main class.
  *
  * @author Koward
+ *         <p>
+ *         modified by Oppahansi
  */
 public class Main {
-
-  private static final String HELP = "(example with Frog.m2 to Classic for one file:)"
-     + "\njava -jar jm2converter.jar -in Frog.m2 -out FrogConverted.m2 -cl\n"
-     + "for a whole folder:"
-     + "\njava -jar jm2converter.jar -in folder/path/ -out folder/outputpath/ -f -cl\n"
-    + "the last '/' or '\\' is important! \n\n";
-  private static final Options options;
-  private static final Map<String, Integer> map;
-  private static final String FILE_ENDING_M2 = ".m2";
-
-  static {
-    options = new Options();
-    options.addOption("in", "input", true, "path to input file");
-    options.addOption("out", "output", true, "path to output file");
-    options.addOption("cl", "classic", false, "convert to Classic");
-    options.addOption("bc", "burningcrusade", false, "convert to The Burning Crusade");
-    options.addOption("lbc", "lateburningcrusade", false, "convert to The Burning Crusade (late versions), better for particles");
-    options.addOption("lk", "lichking", false, "convert to Wrath of the Lich King");
-    options.addOption("cata", "cataclysm", false, "convert to Cataclysm");
-    options.addOption("mop", "pandaria", false, "convert to Mists of Pandaria");
-    options.addOption("wod", "draenor", false, "convert to Warlords of Draenor");
-    options.addOption("leg", "legion", false, "convert to Legion (Build 20810)");
-    options.addOption("f", "folder", false, "path to input folder");
-
-    map = new HashMap<>();
-    map.put("classic", M2Format.CLASSIC);
-    map.put("burningcrusade", M2Format.BURNING_CRUSADE);
-    map.put("lateburningcrusade", M2Format.BURNING_CRUSADE + 3);
-    map.put("lichking", M2Format.LICH_KING);
-    map.put("cataclysm", M2Format.CATACLYSM);
-    map.put("pandaria", M2Format.PANDARIA);
-    map.put("draenor", M2Format.DRAENOR);
-    map.put("legion", M2Format.LEGION);
-  }
 
   /**
    * Main method.
@@ -66,45 +31,46 @@ public class Main {
    * @throws Exception
    */
   public static void main(String[] args) throws Exception {
-    System.out.println("[[ Java M2 Converter by Koward v1.0.8b-beta (oppahansi's version)]]");
-    HelpFormatter formatter = new HelpFormatter();
-    CommandLineParser parser = new DefaultParser();
-    CommandLine cmd = parser.parse(options, args);
+    System.out.println(Constants.AUTHOR_MESSAGE);
 
-    if (!cmd.hasOption("input") || !cmd.hasOption("output")) {
-      System.err.println("Error : No input or/and output specified.");
-      formatter.printHelp(HELP, options);
+    HelpFormatter formatter = new HelpFormatter();
+    CommandLineParser cmdParser = new DefaultParser();
+    CommandLine cmd = cmdParser.parse(Constants.CMD_OPTIONS, args);
+
+    if (!cmd.hasOption(Constants.OptionsValue.IN) || !cmd.hasOption(Constants.OptionsValue.OUT)) {
+      System.err.println(Constants.ErrorMessages.ERR_IN_OUT);
+      formatter.printHelp(Constants.HELP, Constants.CMD_OPTIONS);
       System.exit(1);
     }
 
-    if (cmd.hasOption("folder")) {
-      if (!folderExists(cmd.getOptionValue("input"))) {
-        System.err.println("Error : Folder does not exist: " + cmd.getOptionValue("folder"));
-        formatter.printHelp(HELP, options);
+    if (cmd.hasOption(Constants.OptionsValue.F)) {
+      if (!folderExists(cmd.getOptionValue(Constants.OptionsValue.IN))) {
+        System.err.println(Constants.ErrorMessages.ERR_NO_FOLDER + cmd.getOptionValue(Constants.OptionsValue.F));
+        formatter.printHelp(Constants.HELP, Constants.CMD_OPTIONS);
         System.exit(1);
       }
       else {
-        if (!folderExists(cmd.getOptionValue("output"))) {
-          File outputFolder = new File(cmd.getOptionValue("output"));
+        if (!folderExists(cmd.getOptionValue(Constants.OptionsValue.OUT))) {
+          File outputFolder = new File(cmd.getOptionValue(Constants.OptionsValue.OUT));
           outputFolder.mkdir();
           System.out.println("Output folder created: " + outputFolder.getAbsolutePath());
         }
-        convert(cmd, cmd.hasOption("folder"));
+        convert(cmd, cmd.hasOption(Constants.OptionsValue.F));
       }
     }
     else {
-      convert(cmd, cmd.hasOption("folder"));
+      convert(cmd, cmd.hasOption(Constants.OptionsValue.F));
     }
   }
 
   private static void convert(CommandLine cmd, boolean isFolder) throws Exception {
     if (isFolder) {
-      proccessFiles(cmd, getFileNamesFromFolder(cmd.getOptionValue("input")), isFolder);
+      proccessFiles(cmd, getFileNamesFromFolder(cmd.getOptionValue(Constants.OptionsValue.IN)), true);
     }
     else {
       ArrayList<String> fileList = new ArrayList<>();
-      fileList.add(cmd.getOptionValue("input"));
-      proccessFiles(cmd, fileList, isFolder);
+      fileList.add(cmd.getOptionValue(Constants.OptionsValue.IN));
+      proccessFiles(cmd, fileList, false);
     }
 
   }
@@ -112,28 +78,34 @@ public class Main {
   private static void proccessFiles(CommandLine cmd, ArrayList<String> fileList, boolean isFolder) throws Exception {
     for (String currentFile : fileList) {
 
-      BlizzardInputStream in = new BlizzardInputStream((isFolder) ? cmd.getOptionValue("input") + currentFile : currentFile);
-      Marshalable obj = (Marshalable) in.readObject();
-      in.close();
-      M2 model;
+      BlizzardInputStream in = new BlizzardInputStream(
+         (isFolder) ?
+            cmd.getOptionValue(Constants.OptionsValue.IN) + currentFile
+            :
+            currentFile);
 
-      if (obj instanceof M2) {
-        model = ((M2) obj);
+      Marshalable marshalableObject = (Marshalable) in.readObject();
+      in.close();
+      M2 m2Model;
+
+      if (marshalableObject instanceof M2) {
+        m2Model = ((M2) marshalableObject);
       }
-      else if (obj instanceof MD21) {
-        model = ((MD21) obj).getM2();
+      else if (marshalableObject instanceof MD21) {
+        m2Model = ((MD21) marshalableObject).getM2();
       }
       else {
         throw new Exception("Unknown structure");
       }
+
       System.out.println(currentFile + " read.");
 
-      int newVersion = convertModel(model, cmd);
+      int newVersion = convertModel(m2Model, cmd);
 
       System.out.println("Conversion completed.");
 
       if (!isFolder) {
-        File newFile = new File(cmd.getOptionValue("output"));
+        File newFile = new File(cmd.getOptionValue(Constants.OptionsValue.OUT));
         if (!newFile.exists()) {
           File newDir = new File(newFile.getParent());
           if (!newDir.exists()) {
@@ -142,17 +114,25 @@ public class Main {
         }
       }
 
-      BlizzardOutputStream out = (isFolder) ? new BlizzardOutputStream(cmd.getOptionValue("output") + currentFile) : new BlizzardOutputStream(cmd.getOptionValue("output")) ;
+      BlizzardOutputStream out = (isFolder) ?
+         new BlizzardOutputStream(cmd.getOptionValue(Constants.OptionsValue.OUT) + currentFile)
+         :
+         new BlizzardOutputStream(cmd.getOptionValue(Constants.OptionsValue.OUT));
+
       if (newVersion == M2Format.LEGION) {
         //Pack the MD20 inside MD21 chunked format
         MD21 pack = new MD21();
-        pack.setM2(model);
+        pack.setM2(m2Model);
         out.writeObject(pack);
       }
       else {
-        out.writeObject(model);
+        out.writeObject(m2Model);
       }
-      System.out.println((isFolder) ? cmd.getOptionValue("output") + currentFile  + " written." : cmd.getOptionValue("output") + " written.");
+      System.out.println(
+         (isFolder) ?
+            cmd.getOptionValue(Constants.OptionsValue.OUT) + currentFile + " written."
+            :
+            cmd.getOptionValue(Constants.OptionsValue.OUT) + " written.");
       out.close();
     }
   }
@@ -161,7 +141,7 @@ public class Main {
     boolean converted = false;
     int oldVersion = model.getVersion();
     int newVersion = oldVersion;
-    for (Entry<String, Integer> entry : map.entrySet()) {
+    for (Entry<String, Integer> entry : Constants.M2_FORMATS.entrySet()) {
       String option = entry.getKey();
       Integer version = entry.getValue();
       if (cmd.hasOption(option)) {
@@ -186,7 +166,7 @@ public class Main {
 
     if (files != null) {
       for (File file : files) {
-        if (file.isFile() && file.getName().toLowerCase().contains(FILE_ENDING_M2)) {
+        if (file.isFile() && file.getName().toLowerCase().contains(Constants.FILE_ENDING_M2)) {
           results.add(file.getName());
         }
       }
@@ -196,8 +176,6 @@ public class Main {
   }
 
   private static boolean folderExists(String pathToFolder) {
-    File testDir = new File(pathToFolder);
-
-    return testDir.exists();
+    return new File(pathToFolder).exists();
   }
 }
